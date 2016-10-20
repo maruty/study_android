602a18257aadd1e3a7919363ca660963811f80b6
柳澤　雄高 <yanagisawa_yutaka@r.recruit.co.jp>
2016-10-20 16:24:21 +0900

    Activityのライフサイクル理解用の資料コードをコミット

--- StudyAplication/app/src/main/java/app/maruty/com/studyaplication/MainActivity.java
+++ StudyAplication/app/src/main/java/app/maruty/com/studyaplication/MainActivity.java
@@ -2,12 +2,56 @@ package app.maruty.com.studyaplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //Activityのライフサイクルのチェックをするために全てのライフサイクルにToastを貼ってある状態
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this.getApplicationContext(),"onCreate", Toast.LENGTH_SHORT).show();
+
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this.getApplicationContext(),"onStart", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(this.getApplicationContext(),"onResume", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this.getApplicationContext(),"onPause", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this.getApplicationContext(),"onStop", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this.getApplicationContext(),"onDestroy", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
