d8c31971a69ac07ecf41724157a37721ae9dad6d
柳澤　雄高 <yanagisawa_yutaka@r.recruit.co.jp>
2016-10-20 18:54:07 +0900

    明示的インテントのサンプル　暗黙的インテントのサンプルをコミット

--- StudyAplication/app/src/main/java/app/maruty/com/studyaplication/MainActivity.java
+++ StudyAplication/app/src/main/java/app/maruty/com/studyaplication/MainActivity.java
@@ -1,20 +1,29 @@
 package app.maruty.com.studyaplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Activityのライフサイクルのチェックをするために全てのライフサイクルにToastを貼ってある状態
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this.getApplicationContext(),"onCreate", Toast.LENGTH_SHORT).show();
-
+        Button button1 = (Button)findViewById(R.id.button01);
+        Button button2 = (Button)findViewById(R.id.button02);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
 
     }
 
@@ -53,5 +62,32 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this.getApplicationContext(),"onDestroy", Toast.LENGTH_SHORT).show();
 
     }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this.getApplicationContext(),"onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.button01:
+                Intent intent1 = new Intent(this , SubActivity.class);
+                intent1.putExtra("data","100");
+                startActivity(intent1);
+                break;
+            case R.id.button02:
+                Uri uri = Uri.parse("http://google.com");
+                Intent intent2 = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent2);
+
+            default:
+                break;
+        }
+
+    }
+
+
+
 
 }
--- StudyAplication/app/src/main/java/app/maruty/com/studyaplication/SubActivity.java
+++ StudyAplication/app/src/main/java/app/maruty/com/studyaplication/SubActivity.java
@@ -1,7 +1,9 @@
 package app.maruty.com.studyaplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String number = intent.getStringExtra("data");
+
+        TextView textView = (TextView) findViewById(R.id.text);
+
+        textView.setText("SubActivity" + number);
     }
 }
